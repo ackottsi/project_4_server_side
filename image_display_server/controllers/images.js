f3a37d2481be59b75d731da7d5b63e3ed117115f
ackottsi <kottsickandrewc@johndeere.com>
2021-03-06 22:24:51 -0600

    initial sequelize Image model created

--- image_display_server/controllers/images.js
+++ image_display_server/controllers/images.js
@@ -1,2 +1,16 @@
 const Image=require('../models').Image;
 const constants = require('../constants');
+
+const getAll = (req, res) => {
+    Image.findAll()
+    .then(images => {
+        res.status(constants.SUCCESS).json(images)
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
+
+module.exports={
+    getAll
+}
