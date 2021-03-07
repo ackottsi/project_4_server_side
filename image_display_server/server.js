e3632ce617bfde4cf141164f2945d00290715c78
ackottsi <kottsickandrewc@johndeere.com>
2021-03-06 23:51:55 -0600

    installed CORS functionality

--- image_display_server/server.js
+++ image_display_server/server.js
@@ -2,11 +2,21 @@ require('dotenv').config();
 const express = require('express');  
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 
 const methodOverride = require('method-override'); 
 
 
 app.use(methodOverride('_method'));
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, //allows session cookies to be sent back and forth
+    optionsSuccessStatus: 200 //legacy browsers
+  }
+
+app.use(cors(corsOptions))
 
 app.use('/images', routes.images)
 
