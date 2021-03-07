5ad2b4e867cc179adf07557d9c1f9358887ef18b
ackottsi <kottsickandrewc@johndeere.com>
2021-03-06 21:27:58 -0600

    added local paths for image files

--- image_display_server/images.js
+++ image_display_server/images.js
@@ -3,18 +3,18 @@ const images=[
         Title: 'pic 1',
         date: 3/6/2020,
         comments: "first picture",
-        url:"url here"
+        url:"/Users/ak64719/Desktop/Photos/IMG_20201105_142537.jpg"
     },
     {
         Title: 'pic 2',
         date: 3/6/1999,
         comments: "second picture",
-        url:"url here"  
+        url:"/Users/ak64719/Desktop/Photos/IMG_20201105_164010.jpg"  
     },
     {
     Title: 'pic 3',
     date: 3/6/2222,
-    comments: "third picture picture",
+    comments: "/Users/ak64719/Desktop/Photos/IMG_20201108_225340.jpg",
     url:"url here"
     }
 ]
