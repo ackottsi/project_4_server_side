61283ad58710e146d776e6b48c4bf90b89aad269
ackottsi <kottsickandrewc@johndeere.com>
2021-03-06 21:09:16 -0600

    commiting change to resolve README directory move

---
+++ image_display_server/images.js
@@ -0,0 +1,20 @@
+const images=[
+    {
+        Title: 'pic 1',
+        date: 3/6/2020,
+        comments: "first picture",
+        url:"url here"
+    },
+    {
+        Title: 'pic 2',
+        date: 3/6/1999,
+        comments: "second picture",
+        url:"url here"  
+    },
+    {
+    Title: 'pic 3',
+    date: 3/6/2222,
+    comments: "third picture picture",
+    url:"url here"
+    }
+]
