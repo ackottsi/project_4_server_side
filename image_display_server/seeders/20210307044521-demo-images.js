cc2ed1c6bc37d08022b84d0474dd361ca7b71335
ackottsi <kottsickandrewc@johndeere.com>
2021-03-06 22:54:42 -0600

    confirmed seed migration works.  data comes through to images dev table

--- image_display_server/seeders/20210307044521-demo-images.js
+++ image_display_server/seeders/20210307044521-demo-images.js
@@ -4,22 +4,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Images', [
       {
-        Title: 'pic 1',
-        date: 3/6/2020,
+        title: 'pic 1',
+        date: '2015-08-07 05:00:01',
         comments: "first picture",
         url:"/Users/ak64719/Desktop/Photos/IMG_20201105_142537.jpg"
     },
     {
-        Title: 'pic 2',
-        date: 3/6/1999,
+        title: 'pic 2',
+        date: '2000-08-07 05:00:01',
         comments: "second picture",
         url:"/Users/ak64719/Desktop/Photos/IMG_20201105_164010.jpg"  
     },
     {
-    Title: 'pic 3',
-    date: 3/6/2222,
-    comments: "/Users/ak64719/Desktop/Photos/IMG_20201108_225340.jpg",
-    url:"url here"
+    title: 'pic 3',
+    date: '2014-08-07 05:00:01',
+    comments:"Third picture",
+    url:"/Users/ak64719/Desktop/Photos/IMG_20201108_225340.jpg"
     }
   ],{});
 },
