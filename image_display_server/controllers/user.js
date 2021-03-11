7dd0a06998322b6231adf5c02110475d477718b7
ackottsi <kottsickandrewc@johndeere.com>
2021-03-11 12:37:44 -0600

    initial code added for user controller

---
+++ image_display_server/controllers/user.js
@@ -0,0 +1,22 @@
+const User = require('../models').User;
+const Image = require('../models').Image;
+
+const constants = require('../constants')
+
+const getUserProfile = (req,res)=>{
+    User.findByPk(req.user.id,{
+        include:[
+            {model:Image}
+        ]
+    })
+    .then(userProfile => {
+        res.status(constants.SUCCESS).json(userProfile)
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
+
+module.exports = {
+    getUserProfile,
+}
