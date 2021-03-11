4f3977d5c876f45d9f9771626c831351ac20efde
ackottsi <kottsickandrewc@johndeere.com>
2021-03-11 12:41:20 -0600

    setup rinitial routes for user model

---
+++ image_display_server/routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+router.get('/profile', ctrl.user.getProfile);
+
+module.exports = router;
