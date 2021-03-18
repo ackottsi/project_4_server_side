28585683aec1ea36b162fc1625ffa115a92cac7f
ackottsi <kottsickandrewc@johndeere.com>
2021-03-17 22:25:08 -0500

    corrected issue with Access control allow origin url.  removed forward slash at end

--- server.js
+++ server.js
@@ -29,18 +29,13 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://imagephotobookgaproject4.surge.sh/");
+  res.setHeader("Access-Control-Allow-Origin", "http://imagephotobookgaproject4.surge.sh");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 })
-
-
-
-
-
 
 
 
